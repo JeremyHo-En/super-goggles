2dbe0e0212ee987b6a2edd27ae560364be81d392
JeremyHo-En <jj88090349@gmail.com>
2025-03-25 14:07:13 +0800

    Update ETH_class.py

--- ETH_class.py
+++ ETH_class.py
@@ -17,18 +17,7 @@ def classify_address(w3, address):
             print('wallet')
             time.sleep(0.2)
             return 'wallet', address
-
-        # Check if it is an ERC-20 token contract
-        ERC20_ABI = '[{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}]'
-        contract = w3.eth.contract(address=address, abi=json.loads(ERC20_ABI))
-        try:
-            contract.functions.totalSupply().call()
-            print("ERC-20")
-            time.sleep(0.2)
-            return 'ERC-20', address
-        except:
-            pass  
-
+            
         # Check if it is an ERC-721 or ERC-1155 NFT contract
         ERC721_INTERFACE_ID = "0x80ac58cd"  # ERC-721
         ERC1155_INTERFACE_ID = "0xd9b67a26"  # ERC-1155
@@ -39,6 +28,17 @@ def classify_address(w3, address):
             if contract.functions.supportsInterface(ERC1155_INTERFACE_ID).call():
                 print("NFT (ERC-1155)")
                 return 'NFT', address
+        except:
+            pass  
+            
+        # Check if it is an ERC-20 token contract
+        ERC20_ABI = '[{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}]'
+        contract = w3.eth.contract(address=address, abi=json.loads(ERC20_ABI))
+        try:
+            contract.functions.totalSupply().call()
+            print("ERC-20")
+            time.sleep(0.2)
+            return 'ERC-20', address
         except:
             pass  
 
